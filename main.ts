cbaca909c92741f27cff5a5e1f9b0d9b50ee16e8
Valtleman <99951458+Valtleman@users.noreply.github.com>
2022-03-14 16:21:40 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,36 @@
-
+input.onButtonPressed(Button.A, function () {
+    serial.writeLine("" + (input.temperature()))
+    serial.writeLine("celsius")
+    basic.showString("" + (input.temperature()))
+    basic.showString("celsius")
+})
+input.onButtonPressed(Button.AB, function () {
+    if (input.temperature() >= 25) {
+        serial.writeLine("No fa falta jaqueta")
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        serial.writeLine("¡Agafa la jaqueta!")
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            . . # . .
+            # . # . .
+            # # # . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.temperature() >= 25) {
+        serial.writeLine("" + (input.temperature()))
+        basic.showIcon(IconNames.Happy)
+    } else {
+        serial.writeLine("" + (input.temperature()))
+        basic.showIcon(IconNames.Sad)
+    }
+})
